8bacbce92ba1148bb72a800e11ea80a4ede4e19c
suliat <Suliat>
2023-03-20 12:25:26 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ *
+ * Return - is always 0
+ */
+
 int main(void) 
 {
-	puts("Programming is like building a multilingual puzzle;\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
