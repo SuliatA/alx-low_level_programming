2d482827cea5fcd875f5a8b6627af63208b8e09b
suliat <Suliat>
2023-03-20 12:59:51 -0700

    c4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - use puts function to print a line
  *
- * Return - always 0
+ * Return: always 0
  */
-int main(void) 
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
