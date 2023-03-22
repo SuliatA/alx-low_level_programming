bec37040c80db912aede509505127e01f7312239
suliat <Suliat>
2023-03-21 19:47:16 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,14 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	/* print lowercase letters */
-	for (ch = ‘a’; ch <= ‘z’; ch++)
-		putchar(ch);
-	putchar('n')
+	
+	char letter;
+
+	for (letter = ‘a’; letter <= ‘z’; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
