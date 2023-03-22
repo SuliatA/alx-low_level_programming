2bfc8c01645632882b43f7d78d2019abc35f3855
suliat <Suliat>
2023-03-21 17:25:01 -0700

    putc

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+
+int main()
+{
+	for ( char c = 'A'; c <= 'Z'; c++ )
+
+	{
+		putchar( _ );
+		putchar( p );
+		putchar( u );
+		putchar( t );
+		putchar( c );
+		putchar( h );
+		putchar( a );
+		putchar( r );
+		putchar( \n );
+
+	}
+
+	return (0);
+
+}
