4ec038b7e8d3902cb43373e25b1160a292cd762e
suliat <Suliat>
2023-03-21 19:23:28 -0700

    al

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/* print lowercase letters */
+	for (ch = ‘a’; ch <= ‘z’; ch++)
+		putchar(ch);
+	putchar('n')
