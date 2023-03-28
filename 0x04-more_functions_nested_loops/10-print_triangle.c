cb0e42100037a938c258c3ec73685372c23b2a2e
suliat <Suliat>
2023-03-27 19:59:39 -0700

    tri

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	for (row = 1; row < size; row++)
+	{
+		for (spaces = size - row; spaces >= 1; spaces++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
