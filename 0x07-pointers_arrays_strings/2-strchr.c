2a08de4d3f01c6418b7f6f319cb4ffb264db1ecd
suliat <Suliat>
2023-04-03 20:33:13 -0700

    strc

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strch -  function that locates a character in a string
+ * @c: char to be located
+ * @s: memory area to be searched
+ * Return: pointer to the first occurrence of the character c in the string s
+ * if c is not found - Null.
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
+}
+
